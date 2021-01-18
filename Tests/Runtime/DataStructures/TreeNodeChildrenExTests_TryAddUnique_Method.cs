0343899751d91ef399262253063212eb3e0037b4
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2021-01-18 21:45:07 +0200

    + tests

---
+++ Tests/Runtime/DataStructures/TreeNodeChildrenExTests_TryAddUnique_Method.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+
+namespace Grabli.DataStructures
+{
+	public partial class TreeNodeChildrenExTests
+	{
+		[TestCase(1,1, false)]
+		[TestCase(1,2, true)]
+		public void CheckTryAddUnique(int first, int second, bool expected)
+		{
+			List<int> added = new List<int>();
+			Mock<TreeNodeChildren<int>> mock = new Mock<TreeNodeChildren<int>>();
+			mock.Setup(children => children.Nodes).Returns(new List<TreeNode<int>>() {});
+			mock.Setup(children => children.Add(It.IsAny<int>())).Callback<int>(added.Add);
+			mock.Setup(children => children.Contains(It.IsAny<int>())).Returns<int>(added.Contains);
+			mock.Object.TryAddUnique(first);
+			bool actual = mock.Object.TryAddUnique(second);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
