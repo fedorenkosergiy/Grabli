caef589bf0565af10abbc9e4df97ef76b19c6a05
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2021-01-19 00:22:34 +0200

    + tests

---
+++ Tests/Runtime/DataStructures/TreeNodeExTests_Root_Method.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Grabli.DataStructures
+{
+	public partial class TreeNodeExTests
+	{
+		[Test]
+		public void CheckRootWhereThereIsNoParent()
+		{
+			Mock<TreeNode<int>> mock = new Mock<TreeNode<int>>();
+			mock.Setup(node => node.Parent).Returns(() => null);
+			var root = mock.Object.Root();
+			Assert.AreSame(root, mock.Object);
+		}
+
+		[Test]
+		public void CheckRootWhereThereIsParent()
+		{
+			Mock<TreeNode<int>> parent = new Mock<TreeNode<int>>();
+			Mock<TreeNode<int>> mock = new Mock<TreeNode<int>>();
+			mock.Setup(node => node.Parent).Returns(() => parent.Object);
+			var root = mock.Object.Root();
+			Assert.AreSame(root, parent.Object);
+		}
+	}
+}
