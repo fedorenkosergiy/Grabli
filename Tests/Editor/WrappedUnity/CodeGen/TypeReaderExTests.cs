098a14992ff96373ce3885b4d507a2d284b93fce
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2021-01-20 20:13:55 +0200

    + tests

---
+++ Tests/Editor/WrappedUnity/CodeGen/TypeReaderExTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+
+namespace Grabli.WrappedUnity.CodeGen
+{
+    public partial class TypeReaderExTests
+    {
+        private Factory CreateFakeFactory()
+        {
+            Mock<Factory> mock = new Mock<Factory>();
+            mock.Setup(factory => factory.CreateTypeConfig<ReadonlyTypeConfig>(It.IsAny<TypeConfigRaw>(),It.IsAny<string>())).Returns<TypeConfigRaw, string>(
+                (raw, guid) =>
+                {
+                    Mock<ReadonlyTypeConfig> configMock = new Mock<ReadonlyTypeConfig>();
+                    configMock.Setup(config => config.Guid).Returns(guid);
+                    return configMock.Object;
+                }
+            );
+            mock.Setup(factory => factory.GetReader()).Returns(new DefaultTypeReader(mock.Object));
+            return mock.Object;
+        }
+    }
+}
---
+++ Tests/Editor/WrappedUnity/CodeGen/TypeReaderExTests_TryRead_Method.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using static Grabli.WrappedUnity.CodeGen.FakeSerializedTypes;
+
+namespace Grabli.WrappedUnity.CodeGen
+{
+    public partial class TypeReaderExTests
+    {
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase(RootTypeGuidApplication, true)]
+        [TestCase(RootTypeGuidScreen, true)]
+        public void CheckTryRead(string guid, bool expectedResult)
+        {
+            using (new FileContext(CreateFakeIOFile()))
+            using (new AssetDatabaseContext(CreateFakeAssetDatabase()))
+            {
+                var reader = new DefaultTypeReader(CreateFakeFactory());
+                bool actual = reader.TryRead(guid, out ReadonlyTypeConfig config);
+                Assert.AreEqual(expectedResult, actual);
+            }
+        }
+    }
+}
