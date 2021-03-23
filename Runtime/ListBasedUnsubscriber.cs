eca27296a16924d1f57d67f151ff96b97f1a8367
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2021-03-24 01:40:39 +0200

    * minor

--- Runtime/ListBasedUnsubscriber.cs
+++ Runtime/ListBasedUnsubscriber.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 
 namespace save
 {
-	public class ListBasedUnsubscriber<T> : IDisposable
-	{
-		private IList<IObserver<T>> observers;
-		private IObserver<T> observer;
+    public class ListBasedUnsubscriber<T> : IDisposable
+    {
+        private IList<IObserver<T>> observers;
+        private IObserver<T> observer;
 
-		public ListBasedUnsubscriber(IList<IObserver<T>> observers, IObserver<T> observer)
-		{
-			this.observers = observers;
-			this.observer = observer;
-		}
+        public ListBasedUnsubscriber(IList<IObserver<T>> observers, IObserver<T> observer)
+        {
+            this.observers = observers;
+            this.observer = observer;
+        }
 
-		public void Dispose()
-		{
-			if (observer != null && observers.Contains(observer))
-				observers.Remove(observer);
-		}
-	}
+        public void Dispose()
+        {
+            observers.Remove(observer);
+        }
+    }
 }
