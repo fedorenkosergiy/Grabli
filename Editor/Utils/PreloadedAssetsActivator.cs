db06ffd3ad098475ac6e045660d4f39a9fa56968
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2021-03-26 20:27:08 +0200

    ^ preloaded assets are loaded after zenject installer

--- Editor/Utils/PreloadedAssetsActivator.cs
+++ Editor/Utils/PreloadedAssetsActivator.cs
@@ -8,20 +8,26 @@ namespace Grabli.Utils
 	{
 		static PreloadedAssetsActivator()
 		{
-			EditorApplication.playModeStateChanged += DoOnPlay;
+            ActivatePreloadedAssets();
+            EditorApplication.playModeStateChanged += DoOnPlay;
 		}
 
 		private static void DoOnPlay(PlayModeStateChange state)
-		{
-			if (state != PlayModeStateChange.EnteredPlayMode)
-			{
-				return;
-			}
-			Object[] assets = PlayerSettings.GetPreloadedAssets();
-			for (int i = 0; i < assets.Length; ++i)
-			{
-				assets[i].LogFormat($"PreloadedAssets[{i}]: {{0}}");
-			}
-		}
-	}
+        {
+            if (state != PlayModeStateChange.EnteredPlayMode)
+            {
+                return;
+            }
+            ActivatePreloadedAssets();
+        }
+
+        private static void ActivatePreloadedAssets()
+        {
+            Object[] assets = PlayerSettings.GetPreloadedAssets();
+            for (int i = 0; i < assets.Length; ++i)
+            {
+                assets[i].LogFormat($"PreloadedAssets[{i}]: {{0}}");
+            }
+        }
+    }
 }
