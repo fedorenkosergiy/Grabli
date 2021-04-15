4137a5580e638e2c64108fd91264466fb4cc12d0
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2021-04-15 03:43:03 +0300

    ^ android app restart

--- Runtime/Utils/RestartUtil.cs
+++ Runtime/Utils/RestartUtil.cs
@@ -14,14 +14,33 @@ namespace Grabli.Runtime.Utils
             }
         }
 
+        /// <summary>
+        /// https://noodle1983.github.io/2018/11/17/RestartAndroidAppInUnityPureC/
+        /// </summary>
         public void DoAndroidRestart()
         {
 #if UNITY_ANDROID
-            AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            AndroidJavaObject mainActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
-            mainActivity.Call("doRestart", 0);
-            jc.Dispose();
-            mainActivity.Dispose();
+            using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+                AndroidJavaObject pm = currentActivity.Call<AndroidJavaObject>("getPackageManager");
+                AndroidJavaObject intent = pm.Call<AndroidJavaObject>("getLaunchIntentForPackage", Application.identifier);
+                intent.Call<AndroidJavaObject>("setFlags", 0x20000000);//Intent.FLAG_ACTIVITY_SINGLE_TOP
+
+                AndroidJavaClass pendingIntent = new AndroidJavaClass("android.app.PendingIntent");
+                AndroidJavaObject contentIntent = pendingIntent.CallStatic<AndroidJavaObject>("getActivity", currentActivity, 0, intent, 0x8000000); //PendingIntent.FLAG_UPDATE_CURRENT = 134217728 [0x8000000]
+                AndroidJavaObject alarmManager = currentActivity.Call<AndroidJavaObject>("getSystemService", "alarm");
+                AndroidJavaClass system = new AndroidJavaClass("java.lang.System");
+                long currentTime = system.CallStatic<long>("currentTimeMillis");
+                alarmManager.Call("set", 1, currentTime + 500, contentIntent); // android.app.AlarmManager.RTC = 1 [0x1]
+
+                currentActivity.Call("finish");
+
+                AndroidJavaClass process = new AndroidJavaClass("android.os.Process");
+                int pid = process.CallStatic<int>("myPid");
+                process.CallStatic("killProcess", pid);
+            }
 #endif
         }
     }
