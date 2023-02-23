f61c0cea3f1008a3b5935f92f3ce6c80c1efa7bb
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2023-02-23 23:42:10 +0200

    fixed null check for Unity objects

--- Runtime/Extensions/ObjectEx.cs
+++ Runtime/Extensions/ObjectEx.cs
@@ -1,23 +1,29 @@
 using UnityEngine;
 using System;
+using Object = UnityEngine.Object;
 
 public static class ObjectEx
 {
-	public static bool IsNull<T>(this T value) where T : class
-	{
-		return value == null;
-	}
+    public static bool IsNull<T>(this T value) where T : class
+    {
+        return !Is(value);
+    }
 
-	public static bool Is<T>(this T value) where T : class
-	{
-		return value != null;
-	}
+    public static bool Is<T>(this T value) where T : class
+    {
+        if (value is Object unityObject)
+        {
+            return unityObject;
+        }
 
-	public static T Log<T>(this T value)
-	{
-		Debug.Log(GetValidLog(value));
-		return value;
-	}
+        return value != null;
+    }
+
+    public static T Log<T>(this T value)
+    {
+        Debug.Log(GetValidLog(value));
+        return value;
+    }
 
     public static T Warning<T>(this T value)
     {
@@ -25,23 +31,23 @@ public static class ObjectEx
         return value;
     }
 
-	private static object GetValidLog(object value)
-	{
-		return value ?? "NULL";
-	}
+    private static object GetValidLog(object value)
+    {
+        return value ?? "NULL";
+    }
 
-	public static T LogFormat<T>(this T value, string template)
-	{
-		string log = string.Format(template, GetValidLog(value));
-		Debug.Log(log);
-		return value;
-	}
+    public static T LogFormat<T>(this T value, string template)
+    {
+        string log = string.Format(template, GetValidLog(value));
+        Debug.Log(log);
+        return value;
+    }
 
-	public static void TryThrowArgumentNullException(this object value, string name)
-	{
-		if (value.IsNull())
-		{
-			throw new ArgumentNullException(name);
-		}
-	}
+    public static void TryThrowArgumentNullException(this object value, string name)
+    {
+        if (value.IsNull())
+        {
+            throw new ArgumentNullException(name);
+        }
+    }
 }
