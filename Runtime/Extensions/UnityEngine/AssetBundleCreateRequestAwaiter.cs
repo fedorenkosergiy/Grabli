2da93d105ef7849e7855fafb4a0c9131916c7586
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2023-03-25 11:48:31 +0200

    removed extensions

--- Runtime/Extensions/UnityEngine/AssetBundleCreateRequestAwaiter.cs
+++
@@ -1,9 +0,0 @@
-namespace UnityEngine
-{
-	internal class AssetBundleCreateRequestAwaiter : AsyncOperationAwaiter<AssetBundleCreateRequest, AssetBundle>, AssetBundleAwaiter
-	{
-		public AssetBundleCreateRequestAwaiter(AssetBundleCreateRequest request) : base(request) { }
-
-		public override AssetBundle GetResult() => request.assetBundle;
-	}
-}
--- Runtime/Extensions/UnityEngine/AssetBundleRequestAwaiter.cs
+++
@@ -1,9 +0,0 @@
-namespace UnityEngine
-{
-	internal class AssetBundleRequestAwaiter : AsyncOperationAwaiter<AssetBundleRequest, Object>, ObjectAwaiter
-	{
-		public AssetBundleRequestAwaiter(AssetBundleRequest request) : base(request) { }
-
-		public override Object GetResult() => request.asset;
-	}
-}
